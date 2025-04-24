4c742ef9dc8b2001172c7bb419835442877620eb
laura.p.santos8@aluno.senai.br <laura.p.santos8@aluno.senai.br>
2025-04-24 16:00:57 -0300

    first commit

---
+++ operacoes.go
@@ -0,0 +1,17 @@
+package main 
+
+func soma(x, y int) int { 
+return x + y 
+}
+
+func subtrair(x, y int) int{ 
+return x - y
+}
+ 
+func multiplicar(x, y int) int{
+	return x * y
+}
+
+func dividir(x, y int) int{
+	return x / y
+}
